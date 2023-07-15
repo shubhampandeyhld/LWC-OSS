324e0d04d7b10442f82d55766c4fdae9eb473761
Shubham Pandey <Shubham.Pandey@blackbaud.me>
2023-07-15 11:57:11 +0530

    js

--- src/modules/example/app/app.js
+++ src/modules/example/app/app.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorldApp extends LightningElement {}
+export default class HelloWorldApp extends LightningElement {
+    connectedCallback(){
+        console.log('site loaded')
+    }
+}
