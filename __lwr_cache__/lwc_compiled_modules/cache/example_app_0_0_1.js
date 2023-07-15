bda37de2dcad52c35e60628f129ad699d185c19b
Shubham Pandey <Shubham.Pandey@blackbaud.me>
2023-07-15 11:28:50 +0530

    first commit

---
+++ __lwr_cache__/lwc_compiled_modules/cache/example_app_0_0_1.js
@@ -0,0 +1,10 @@
+import _tmpl from "./app.html";
+import { registerComponent as _registerComponent, LightningElement } from "lwc";
+class HelloWorldApp extends LightningElement {
+  /*LWC compiler v3.0.0*/
+}
+export default _registerComponent(HelloWorldApp, {
+  tmpl: _tmpl,
+  sel: "example-app",
+  apiVersion: 59
+});
---
+++ __lwr_cache__/lwc_compiled_modules/cache/example_app_app_html_0_0_1.js
@@ -0,0 +1,25 @@
+import { freezeTemplate } from "lwc";
+
+import _implicitStylesheets from "./app.css";
+
+import _implicitScopedStylesheets from "./app.scoped.css?scoped=true";
+
+import {parseFragment, registerTemplate} from "lwc";
+const $fragment1 = parseFragment`<main${3}><img src="/public/assets/recipes-logo.png" alt="logo"${3}><h1${3}>Hello World!</h1></main>`;
+function tmpl($api, $cmp, $slotset, $ctx) {
+  const {st: api_static_fragment} = $api;
+  return [api_static_fragment($fragment1(), 1)];
+  /*LWC compiler v3.0.0*/
+}
+export default registerTemplate(tmpl);
+tmpl.stylesheets = [];
+
+
+if (_implicitStylesheets) {
+  tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitStylesheets);
+}
+if (_implicitScopedStylesheets) {
+  tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitScopedStylesheets);
+}
+tmpl.stylesheetToken = "lwc-5la2ic78dit";
+freezeTemplate(tmpl);
